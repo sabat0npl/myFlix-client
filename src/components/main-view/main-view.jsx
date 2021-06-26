e22ff6a12919a7a3d9024ed3cd0cd8ef67d17f13
Bruno Zablotny <rusholdfield@gmail.com>
2021-06-26 22:18:45 +0200

    Added login and registration view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,83 +1,73 @@
-import React from 'react';
-import {
-  MovieCard
-} from '../movie-card/movie-card';
-import {
-  MovieView
-} from '../movie-view/movie-view';
+import React from "react";
+import axios from "axios";
 
-export class MainView extends React.Component {
+import { LoginView } from "../login-view/login-view";
+import { RegistrationView } from "../registration-view/registration-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+class MainView extends React.Component {
   constructor() {
-    super(); // refers to OOP, means call the constructor of the parent class, in this case 'React.Component'
+    super();
     this.state = {
-      movies: [{
-          _id: 1,
-          title: 'Inception',
-          description: 'Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people\'s dreams and steal their secrets from their subconscious. His skill has made him a hot commodity in the world of corporate espionage but has also cost him everything he loves. Cobb gets a chance at redemption when he is offered a seemingly impossible task: Plant an idea in someone\'s mind. If he succeeds, it will be the perfect crime, but a dangerous enemy anticipates Cobb\'s every move.',
-          imagePath: 'https://resizing.flixster.com/4MrL62heb7yBgBt8zllSeqNZxg4=/206x305/v2/https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg'
-        },
-        {
-          _id: 2,
-          title: 'The Shawshank Redemption',
-          description: 'Andy Dufresne (Tim Robbins) is sentenced to two consecutive life terms in prison for the murders of his wife and her lover and is sentenced to a tough prison. However, only Andy knows he didn\'t commit the crimes. While there, he forms a friendship with Red (Morgan Freeman), experiences brutality of prison life, adapts, helps the warden, etc., all in 19 years.',
-          imagePath: 'https://resizing.flixster.com/U8DOCAyL0efMS6cA0UmrNi8oyQk=/206x305/v2/https://flxt.tmsimg.com/NowShowing/3725/3725_aa.jpg'
-        },
-        {
-          _id: 3,
-          title: 'Gladiator',
-          description: 'Commodus (Joaquin Phoenix) takes power and strips rank from Maximus (Russell Crowe), one of the favored generals of his predecessor and father, Emperor Marcus Aurelius, the great stoical philosopher. Maximus is then relegated to fighting to the death in the gladiator arenas.',
-          imagePath: 'https://resizing.flixster.com/z1uxBIn8PvwL-9RdEvzLbHJbc9Y=/206x305/v2/https://flxt.tmsimg.com/assets/p24674_p_v13_bc.jpg'
-        }
-      ],
-      selectedMovie: null
-    }
+      movies: [],
+      selectedMovie: null,
+      user: null,
+    };
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  componentDidMount() {
+    axios
+      .get("https://brunoza-api.herokuapp.com/movies")
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  /* When a movie is clicked, this function is involed and updates the state of the selectedMovie property to that movie*/
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie,
     });
   }
 
+  /*When a user logs in, this function updates the user property in state to that particular user */
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
+  onRegister(register) {
+    this.setState({
+      register,
+    });
+  }
 
   render() {
-    const {
-      movies,
-      selectedMovie
-    } = this.state;
+    const { movies, selectedMovie, user } = this.state;
 
-    if (movies.length === 0) return <div class = "main-view" > 'The list is empty' < /div>;
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
-      // <React.Fragment> or <>
-      <
-      div className = "main-view" > {
-        selectedMovie ?
-        < MovieView movieData = {
-          selectedMovie
+      <div className="main-view">
+        {selectedMovie 
+        ? <MovieView movie={selectedMovie} onBackClick={(newSelectedMovie) => {this.setSelectedMovie(newSelectedMovie); }} />
+        : movies.map((movie) => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+          ))
         }
-        onBackClick = {
-          newSelectedMovie => {
-            this.setSelectedMovie(newSelectedMovie);
-          }
-        }
-        /> :
-          movies.map(movie => ( <
-          MovieCard key = {
-            movie._id
-          }
-          movieData = {
-            movie
-          }
-          onMovieClick = {
-            (movie) => {
-              this.setSelectedMovie(movie)
-            }
-          }
-          />
-        ))
-      }
       </div>
     );
   }
-}
+};
+
+export default MainView;
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 
 export class MovieView extends React.Component {
-    render () {
-        const { movieData, onBackClick } = this.props;
+  render() {
+    const { movie, onBackClick } = this.props;
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.imageUrl} />
+        </div>
+        <div className="movie-title">
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="value">{movie.description}</span>
+        </div>
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}
+        >
+          Back to list
+        </button>
+      </div>
+    );
+  }
+}
 
-        return (
-            <div className="movie-view">
-                <div className="movie-poster">
-                    <img src={movieData.imagePath} />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{ movieData.title }</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{ movieData.description }</span>
-                </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
-        );
-    }
+export class MainView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+    };
+  }
 }
